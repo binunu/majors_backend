f99c3a3c71b3e3deaebc1617c035ad57a9160aaa
[binunu] <[tjdqls12271@gmail.com]>
2023-12-01 05:16:54 +0900

    git init

---
+++ src/main/java/com/binunu/majors/MajorsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.binunu.majors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajorsBackendApplication.class, args);
+        System.out.print("git init");
+    }
+}
---
+++ src/test/java/com/binunu/majors/MajorsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binunu.majors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
